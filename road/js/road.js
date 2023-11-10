e860b323714f388698f170a16684729666a6a72e
hqh <642134542@qq.com>
2023-11-10 18:01:36 +0800

    update: 增加点位更新的方法

--- road/js/road.js
+++ road/js/road.js
@@ -378,6 +378,61 @@ export default class DoubleTrack {
     }
   }
 
+  updatePoint(list) {
+    let newPoints = [];
+    let vec = null;
+    for (let i = 1; i < list.length; i += 1) {
+      if (i === 0) {
+        newPoints.push(arr[i]);
+      } else if (i >= 1) {
+        if (arr[i].type === 'lineRoad') {
+          let { direction, distance } = this.getInfoBytwoPoint(
+            arr[i - 1],
+            arr[i]
+          );
+          const endPoint = getPointByVector(arr[i - 1], direction, distance);
+          endPoint.push(endPoint)
+        } else {
+          const { nextStartPoint } = this.createBend(
+            arr[i - 2].point,
+            arr[i - 1].point,
+            this.width,
+            arr[i].mouseDirection === 'right',
+            vec
+          );
+          endPoint.push(nextStartPoint);
+        }
+        if (arr[i].type === 'lineRoad') {
+          vec = direction;
+        } else {
+          const verticalVector = this.getVerticalVector(vec);
+          const angle = Math.PI / 2;
+          if (arr[i].mouseDirection === 'right') {
+            const quaternion = new THREE.Quaternion().setFromAxisAngle(
+              verticalVector,
+              angle
+            );
+            const rotatedVec = verticalVector.clone().applyQuaternion(quaternion); // 绕轴旋转后的向量
+            // 给下一次的初始旋转向量赋值
+            vec = rotatedVec;
+          } else {
+            const reverseVerticalVector = verticalVector.clone().negate();
+            const angle = Math.PI / 2;
+            const quaternion = new THREE.Quaternion().setFromAxisAngle(
+              reverseVerticalVector,
+              angle
+            );
+            // const v = new THREE.Vector3(1,0,0); // 原始向量
+            const rotatedVec = reverseVerticalVector
+              .clone()
+              .applyQuaternion(quaternion); // 绕轴旋转后的向量
+            // 旋转
+            vec = rotatedVec;
+          }
+        }
+      }
+    }
+  }
   onKeyDown(event) {
     if (event.ctrlKey) {
       this.ctrlDown = true;
